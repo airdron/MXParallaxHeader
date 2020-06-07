3fcbf5bd26242bbe83b8e186227c24b82c7fba8f
Andrew Oparin <a.oparin@corp.mail.ru>
2020-06-07 19:02:23 +0300

    attempt 4

--- MXParallaxHeader/MXScrollView.m
+++ MXParallaxHeader/MXScrollView.m
@@ -64,9 +64,7 @@ static void * const kMXScrollViewKVOContext = (void*)&kMXScrollViewKVOContext;
     self.showsVerticalScrollIndicator = NO;
     self.directionalLockEnabled = YES;
     self.bounces = YES;
-    
-    self.panGestureRecognizer.cancelsTouchesInView = NO;
-    
+        
     self.observedViews = [NSMutableArray array];
     
     [self addObserver:self forKeyPath:NSStringFromSelector(@selector(contentOffset))
@@ -180,7 +178,7 @@ static void * const kMXScrollViewKVOContext = (void*)&kMXScrollViewKVOContext;
                 
             } else if (self.contentOffset.y < -self.contentInset.top && !self.bounces) {
                 [self scrollView:self setContentOffset:CGPointMake(self.contentOffset.x, -self.contentInset.top)];
-            } else if (self.contentOffset.y > -self.parallaxHeader.minimumHeight) {
+            } else if (self.contentOffset.y > -ceil(self.parallaxHeader.minimumHeight)) {
                 [self scrollView:self setContentOffset:CGPointMake(self.contentOffset.x, -self.parallaxHeader.minimumHeight)];
             }
             
